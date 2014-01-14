684eeb4f6491f17d43950a38ca66ca17c9f8b150
girish swaminathan <girish1290@gmail.com>
2014-01-14 23:31:22 +0530

    Created application using Refinery CMS 2.1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thenewsminute::Application.config.secret_token = '14ee1e19f291dc4cc2a9567aa7ccaf7e1efbf8ed68cb5c87a1aa7ae83181390ad78e63c4062a52b333d2c2cc91940b3f9a0f2fb89506e69c307370209ce46197'
