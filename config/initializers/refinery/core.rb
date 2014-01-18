fb9700ea0820e2dfc041b40e04440f5d5a0d8ef7
girish swaminathan <girish1290@gmail.com>
2014-01-18 16:37:40 +0530

    admin changes

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -20,7 +20,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  # config.site_name = "Company Name"
+  config.site_name = "The Newsline"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
@@ -44,5 +44,5 @@ Refinery::Core.configure do |config|
   # config.register_stylesheet "custom", :media => 'screen'
 
   # Specify a different backend path than the default of /refinery.
-  # config.backend_route = "refinery"
+  config.backend_route = "admin"
 end
