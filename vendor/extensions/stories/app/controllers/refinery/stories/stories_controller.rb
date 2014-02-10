0433725e9f6a8c3be56db2257fa770d150f4f367
girish swaminathan <girish1290@gmail.com>
2014-02-11 01:27:07 +0530

    stories page changes

--- vendor/extensions/stories/app/controllers/refinery/stories/stories_controller.rb
+++ vendor/extensions/stories/app/controllers/refinery/stories/stories_controller.rb
@@ -13,6 +13,7 @@ module Refinery
 
       def show
         @story = Story.find(params[:id])
+           @mainpage = Refinery::Mainpages::Mainpage.find(1)
 
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @story in the line below:
