f706955a861cb04ce7a585c440cb20dea54e7ffb
girish swaminathan <girish1290@gmail.com>
2014-06-02 10:44:30 +0530

    backend change

--- vendor/extensions/politics/app/controllers/refinery/politics/admin/politics_controller.rb
+++ vendor/extensions/politics/app/controllers/refinery/politics/admin/politics_controller.rb
@@ -5,6 +5,7 @@ module Refinery
 
         crudify :'refinery/politics/politic',
                 :title_attribute => 'Main_headline',
+                :per_page => '1000',
                 :xhr_paging => true
 
       end
